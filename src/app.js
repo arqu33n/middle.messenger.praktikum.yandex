6da567930c92572a67eb83f9b8ccdd84010a2979
arqu33n <soondawn@yandex.ru>
2023-03-11 22:02:11 +0300

    add express

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import { loginLink } from "./modules/login/loginTpl";
 import { signinLink } from "./modules/signin/signinTpl";
+import { profileLink } from "./modules/profile/profileTpl";
+import { chatLink } from "./modules/chat/chatTpl";
+import { errorLink } from "./modules/error/notFoundTpl";
 
 export const App = () => {
   switch (window.location.pathname) {
@@ -7,6 +10,23 @@ export const App = () => {
       return signinLink();
     case "/login":
       return loginLink();
+    case "/profile":
+      return profileLink();
+    case "/chat":
+    case "/chat/":
+      return chatLink();
+    case "/not-found":
+      return errorLink({
+        errorText: "Назад к чатам",
+        errorMessage: "Не туда попали",
+        errorCode: "404",
+      });
+    case "/error":
+      return errorLink({
+        errorText: "Назад к чатам",
+        errorMessage: "Мы уже фиксим",
+        errorCode: "500",
+      });
     default:
       return loginLink();
   }
